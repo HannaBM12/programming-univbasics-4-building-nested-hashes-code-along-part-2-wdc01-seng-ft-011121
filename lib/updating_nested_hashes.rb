6377b1907075ef15b0224eb95f893d55c798cece
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-29 17:02:08 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,16 +61,16 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus"
    #   - The second hash should have the :name key set to "Apothecary"
    # Code your solution here:
-
-  # epic_tragedy[:additional_characters][0] = {name: "Prince Escalus"}
-  # epic_tragedy[:additional_characters][1] = {name: "Apothecary"}
-    epic_tragedy[:additional_characters] = Array.new
-
-    prince = {:name => "Prince Escalus"}
-    apothecary = {:name => "Apothecary"}
-
-    epic_tragedy[:additional_characters][0] = prince
-    epic_tragedy[:additional_characters][1] = apothecary
+  epic_tragedy[:additional_characters] = []
+  epic_tragedy[:additional_characters][0] = {name: "Prince Escalus"}
+  epic_tragedy[:additional_characters][1] = {name: "Apothecary"}
+    # epic_tragedy[:additional_characters] = Array.new
+    #
+    # prince = {:name => "Prince Escalus"}
+    # apothecary = {:name => "Apothecary"}
+    #
+    # epic_tragedy[:additional_characters][0] = prince
+    # epic_tragedy[:additional_characters][1] = apothecary
 
 
    # After your code, the following line will return the altered hash
